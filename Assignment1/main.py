dcf4affe59660a5221f40733efde5a26383ab229
M7md Odeh <1200089@student.birzeit.edu>
2023-11-30 10:18:33 +0200

    .

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -33,7 +33,6 @@ print('----------------------------------------------------------------------')
 # Check for missing values in each feature
 temp = cars_data.isnull().sum()
 missing_values = temp[temp > 0]
-
 if missing_values.empty:
     print("No, there are no missing values in the dataset.")
 else:
@@ -44,13 +43,15 @@ print('----------------------------------------------------------------------')
 
 ############################## part 3 ########################################
 
-# Filling missing values for numeric columns with median
-numeric_columns = cars_data.select_dtypes(include=['number']).columns
-cars_data[numeric_columns] = cars_data[numeric_columns].fillna(cars_data[numeric_columns].median())
-
-# Filling missing values for non-numeric column 'origin' with mode
-mode_origin = cars_data['origin'].mode()[0]
-cars_data['origin'].fillna(mode_origin, inplace=True)
+for column in cars_data.columns:
+    if pd.api.types.is_numeric_dtype(cars_data[column]):
+        # Filling missing values for numeric columns with median
+        median_value = cars_data[column].median()
+        cars_data[column].fillna(median_value, inplace=True)
+    else:
+        # Filling missing values for non-numeric column 'origin' with mode
+        mode_value = cars_data[column].mode()[0]
+        cars_data[column].fillna(mode_value, inplace=True)
 
 # Check if any missing values remain
 print("Number of missing values after filling:")
@@ -63,7 +64,7 @@ print('----------------------------------------------------------------------')
 best_fuel_economy_country = cars_data.groupby('origin')['mpg'].mean().idxmax()
 print("Country with the best fuel economy: ", best_fuel_economy_country)
 
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(11, 7))
 sns.boxplot(x='origin', y='mpg', data=cars_data)
 
 plt.title('Fuel Economy (MPG) by Country of Origin')
@@ -156,11 +157,11 @@ X = np.hstack((np.ones((X.shape[0], 1)), X))  # Adding x0 = 1 for the intercept
 y = cars_data['mpg'].values.reshape(-1, 1)  # Target variable
 
 # Closed form solution of linear regression: (X'X)^-1 X'y
-X_transpose = X.T
-theta = np.linalg.inv(X_transpose.dot(X)) @ X_transpose @ y  # Theta (parameters)
+XT = X.T
+w = np.linalg.inv(XT.dot(X)) @ XT @ y  # w (parameters)
 
 # Predicting 'mpg' from 'horsepower' using the learned model
-y_pred = X @ theta
+y_pred = X @ w
 
 # Plotting the scatter plot and the learned linear regression line
 plt.figure(figsize=(10, 6))
@@ -180,21 +181,23 @@ print('----------------------------------------------------------------------')
 # Generating horsepower values for the quadratic curve
 horsepower_range = np.linspace(cars_data['horsepower'].min(), cars_data['horsepower'].max(), 500)
 horsepower_range_reshaped = horsepower_range.reshape(-1, 1)
-X_quad_range = np.hstack((np.ones(horsepower_range_reshaped.shape), horsepower_range_reshaped, horsepower_range_reshaped ** 2))
+# Creates a matrix for the quadratic model with three columns: [1, horsepower, horsepower^2]
+z_range = np.hstack((np.ones(horsepower_range_reshaped.shape), horsepower_range_reshaped, horsepower_range_reshaped ** 2))
 
-# Correcting the dimensions of X_quad
-X_quad = np.hstack((np.ones((X.shape[0], 1)), X[:, 1].reshape(-1, 1), (X[:, 1] ** 2).reshape(-1, 1)))
+# Create z matrix from X matrix for the quadratic model with three columns: [1, horsepower, horsepower^2]
+z = np.hstack((np.ones((X.shape[0], 1)), X[:, 1].reshape(-1, 1), (X[:, 1] ** 2).reshape(-1, 1)))
 
-# Calculating theta_quad for the quadratic model
-theta_quad = np.linalg.inv(X_quad.T.dot(X_quad)) @ X_quad.T @ y
+# Calculating w_quad for the quadratic model
+zT= z.T
+w_quadratic = np.linalg.inv(zT.dot(z)) @ zT @ y
 
 # Predicting 'mpg' for the generated horsepower range using the quadratic model
-y_pred_quad_range = X_quad_range @ theta_quad
+ypred_quadratic = z_range @ w_quadratic
 
 # Plotting the scatter plot with the quadratic regression curve
 plt.figure(figsize=(10, 6))
 plt.scatter(cars_data['horsepower'], cars_data['mpg'], label='Data points')
-plt.plot(horsepower_range, y_pred_quad_range, color='green', label='Quadratic Regression')
+plt.plot(horsepower_range, ypred_quadratic, color='green', label='Quadratic Regression')
 
 plt.title('Horsepower vs MPG with Quadratic Regression Curve')
 plt.xlabel('Horsepower')
@@ -206,52 +209,48 @@ print('----------------------------------------------------------------------')
 
 ############################## part 10 ########################################
 
-# Function to compute the mean squared error
-def compute_cost(X, y, theta):
-    m = len(y)
-    predictions = X.dot(theta)
-    cost = (1/(2*m)) * np.sum(np.square(predictions - y))
-    return cost
 
 # Function to perform gradient descent
-def gradient_descent(X, y, theta, learning_rate, num_iterations):
+def gradient_descent(X, y, w, learning_rate, num_iterations):
     m = len(y)
-    cost_history = np.zeros(num_iterations)
 
     for i in range(num_iterations):
-        predictions = X.dot(theta)
-        theta -= (1/m) * learning_rate * X.T.dot(predictions - y)
-        cost_history[i] = compute_cost(X, y, theta)
+        predictions = X.dot(w)
+        w -= (1/m) * learning_rate * X.T.dot(predictions - y)
+    return w
 
-    return theta, cost_history
 
-# Initialize variables
-theta = np.zeros((2, 1))  # 2 parameters (intercept and slope)
-iterations = 10000
-alpha = 0.001  # Learning rate
-
-# Preparing the data
+# Extracting features and target variable
 X = cars_data['horsepower'].values.reshape(-1, 1)
-X = np.hstack((np.ones((X.shape[0], 1)), X))  # Adding x0 = 1 for the intercept
 y = cars_data['mpg'].values.reshape(-1, 1)
 
+# Normalize the 'horsepower' feature using z-score normalization
+mean_horsepower = np.mean(X)
+std_horsepower = np.std(X)
+X = (X - mean_horsepower) / std_horsepower
+
+# Add intercept term to feature matrix
+X = np.hstack((np.ones((X.shape[0], 1)), X))
+
 # Initializing parameters
-theta = np.zeros((2, 1))  # 2 parameters (intercept and slope)
-learning_rate = 0.01
-num_iterations = 1000
+w = np.zeros((2, 1))  # 2 parameters (intercept and slope)
+learning_rate = 0.01  # Adjust the learning rate
+num_iterations = 1000  # Adjust the number of iterations
+
 
 # Performing gradient descent
-theta, cost_history = gradient_descent(X, y, theta, learning_rate, num_iterations)
+w = gradient_descent(X, y, w, learning_rate, num_iterations)
 
-# Print the final theta
-print("Theta after Gradient Descent:", theta)
+# Print the final w values
+print("w after Gradient Descent:", w)
 
 
 # Generate predictions using the learned model
-predictions = X.dot(theta)
+predictions = X.dot(w)
 
 # Plot the data and the regression line
-plt.scatter(X[:, 1], y, color='blue', label='Data Points')  # Original data
+plt.figure(figsize=(10, 6))
+plt.scatter(X[:, 1], y, label='Data Points')  # Original data
 plt.plot(X[:, 1], predictions, color='red', label='Regression Line')  # Regression line
 plt.title('Horsepower vs MPG')
 plt.xlabel('Horsepower')
--- Assignment1/tempCodeRunnerFile.py
+++ Assignment1/tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
- 
+# Normalize the 'horsepower' feature using z-score normalization
+mean_horsepower = np.mean(X)
+std_horsepower = np.std(X)
+X = (X - mean_horsepower) / std_horsepower
